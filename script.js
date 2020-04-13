ae2d4c38d9393780db732138a709f488c5d7f6c8
FaraazKhhan <codefaraaz@gmail.com>
2020-04-13 23:20:25 +0530

    color picker and script added

---
+++ script.js
@@ -0,0 +1,16 @@
+var colorPicker1 = document.getElementById('colorP1');
+var colorPicker2 = document.getElementById('colorP2');
+
+var colorValue1 = document.getElementById('colorValue1');
+var colorValue2 = document.getElementById('colorValue2');
+
+var changeBtn = document.getElementById('changeColor');
+
+function colorValueF1(){
+	var cValue1 = colorPicker1.value;
+	colorValue1.innerHTML = "Hello";
+}
+
+function changeColor(){
+
+}
